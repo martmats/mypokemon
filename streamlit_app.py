ae78a4a65715531fdb013214c8fc0420e18f4e2d
Marta <martamatiasroca@gmail.com>
2024-06-24 13:34:01 +0000

    Fetch data and add the features of each Pokemon I want to display

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,20 @@
-import streamlit as st
-import pandas as pd
-import requests
+# Function to fetch Pokémon data (typical ones + sound + abilites and type)
+def get_pokemon_data(pokemon_number):
+    pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_number}"
+    response = requests.get(pokemon_url).json()
+    return {
+        'name': response['name'].title(),
+        'height': response['height'] / 10,  # height is given in decimetres
+        'weight': response['weight'] / 10,  # weight is given in hectograms
+        'image_url': response['sprites']['front_default'],
+        'types': [t['type']['name'].title() for t in response['types']],
+        'abilities': [a['ability']['name'].title() for a in response['abilities']],
+        'cry_url': f"https://pokemoncries.com/cries/{pokemon_number}.mp3"  # URL for the Pokemon sound
+    }
 
-st.title('Pokemon Explorer!!!')
+# Display a grid of Pokémon images
+num_pokemon = 20  # I can adjust the number of pokemon to display (there are like 1000ish)
+columns = 10  # Number of columns for the images
+selected_pokemon = None
 
-
-### element to pick the pokemon number!!
-pokemon_number = st.slider("Choose a pokemon!!!", 1, 1100)
-
-## element to get the latest data on that pokemon!
-pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_number}"
-response = requests.get(pokemon_url).json() 
-
-#element to isolate specific facts about that pokemon!
-pokemon_name = response['name']
-pokemon_height = response['height']
-
-#code to display it! 
-st.title(pokemon_name.title())
-st.write(f"This pokemon is {pokemon_height} meters tall!")
+st.write("### Click on a Pokémon image to see its details")
